7c9a3cead43e0c66bff15e55013a4b9f66f7fa5d
i-Rich <60166054+i-Rich@users.noreply.github.com>
2023-04-05 10:13:06 -0400

    new branch

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,13 +1,13 @@
 # Specify the provider and region
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
-# Define the EC2 instance
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+# Define the VPC
+resource "aws_vpc" "ations_vs" {
+    cidr_block  = "10.0.0.0/16"
+
   tags = {
-    Name = "Example EC2 Instance"
+    Name = "Class30"
   }
 }
