51843bbecda6462fbab2fa25750487112797b86c
i-Rich <60166054+i-Rich@users.noreply.github.com>
2023-04-06 01:19:02 -0400

    tenth commit

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,7 +1,5 @@
 provider "aws" {    
   region = "us-west-1"
-  access_key = "AKIA4OT2OWDL2WH5IXNW"
-  secret_key = "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"
 }
 
 resource "aws_vpc" "actions_vs" {
